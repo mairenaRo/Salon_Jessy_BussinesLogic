0e30031ee3a548fd3e6c6ab17309e97fde0abde3
mairenaRo <ronny.mairena.r@hotmail.com>
2022-06-09 14:17:16 -0600

    Creación del API para REACT
    Se realiza el proyecto que posee la base del API para comunicar las diferentes capas del sistema.

---
+++ BL_Salon_Jessy/Cita.cs
@@ -0,0 +1,13 @@
+﻿namespace BL_Salon_Jessy
+{
+    public class Cita
+    {
+        public int EstadoID { get; set; }
+        public int ServicioID { get; set; }
+        public int PersonaID { get; set; }
+        public DateTime Fecha { get; set; }
+        public TimeOnly HoraIncio { get; set; }
+        public TimeOnly timeOnly { get; set; }
+
+    }
+}
---
+++ BL_Salon_Jessy/Persona.cs
@@ -0,0 +1,13 @@
+﻿namespace BL_Salon_Jessy
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public int Rol { get; set; }
+        public string Correo {get; set; }
+        public string Contrasenna { get; set; }
+    }
+}
